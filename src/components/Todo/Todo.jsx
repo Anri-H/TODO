4779d9ca305a6f5e78a342f8d72d2e88aae263fb
Anri <anrihunanyan@gmail.com>
2021-11-13 19:29:02 +0400

    fix(Todo.jsx): corrected imports

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { todos } from "../../store/features/todo/todoSlice";
-import Add from "../Add/Add";
-import TodoItem from "../TodoItem/TodoItem";
+import { Add } from "../Add/Add";
+import { TodoItem } from "../TodoItem/TodoItem";
 
 export default function Todo() {
   const todoList = useSelector(todos);
-
   return (
     <div>
       <h1>TODO List</h1>
