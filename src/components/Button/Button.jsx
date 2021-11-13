80cd76c8b216c29fcca1314bac8b0bd435b11081
Anri <anrihunanyan@gmail.com>
2021-11-13 19:27:14 +0400

    fix(Button.jsx): momize component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,10 @@
-export default function Button({ text, onClick, disabled }) {
+import React from "react";
+
+export const Button = React.memo(({ text, onClick, disabled }) => {
+  console.log("button");
   return (
     <button onClick={onClick} disabled={disabled}>
       {text}
     </button>
   );
-}
+});
