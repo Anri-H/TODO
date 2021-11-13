788463dd4e25560dce7a2935add4f338b912dad8
Anri <anrihunanyan@gmail.com>
2021-11-13 16:51:28 +0400

    feat(Todo.jsx): created main todo component

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { todos } from "../../store/features/todo/todoSlice";
+import Add from "../Add/Add";
+import TodoItem from "../TodoItem/TodoItem";
+
+export default function Todo() {
+  const todoList = useSelector(todos);
+
+  return (
+    <div>
+      <h1>TODO List</h1>
+      <Add />
+      <ul>
+        {todoList.map((todo, i) => {
+          return <TodoItem todo={todo} i={i} />;
+        })}
+      </ul>
+    </div>
+  );
+}
