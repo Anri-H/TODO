1cd1dad37d7e79b117f0a4666df4f5a1612f1b0a
Anri Hunanyan <83278887+Anri-H@users.noreply.github.com>
2021-11-14 14:14:47 +0400

    fix(Button.jsx): deleted log

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Button.scss";
 
 export const Button = React.memo(({ text, onClick, disabled }) => {
-  console.log("button");
   return (
     <button className="button" onClick={onClick} disabled={disabled}>
       {text}
